314a8fdabe73e678ef66b6ae074aea1cc66c739c
Fatima Ezzahra <felemenoun@gmail.com>
2023-03-11 19:16:36 +0100

    Update the projects

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -69,22 +69,19 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Ruby on Rails • React • Redux • JavaScript • Ruby
+                REACT • REDUX • JAVASCRIPT • CYPRESS • TAILWIND CSS
                   </div>
                 <h3 className="mt-0 mb-12">
-                Car rental app
+                Change Your World
                   </h3>
                 <p className="m-0">
-                This webapp is allows users to get list of cars, cars details, reserve a car and check their reservations list. If the user is the admin that give them the authorization to delete from or add a new car to the list of car. Users need to sign up or sign in to access the API endponits. They can sign up or sign in using the API endpoints.
+                This is responsive web application for a non-profit organization. It consist of multiple pages some of them require to sign up or sign in or you need to be authorized to access some content. This project uses data that is coming from a RESTful API.
                </p>
                <br />
                <div className="reveal-from-bottom" data-reveal-delay="600">
                   <ButtonGroup>
-                    <Button tag="a" target="_blank" rel="noopener noreferrer" color="primary" wideMobile href="https://car-renta.herokuapp.com/">
+                    <Button tag="a" target="_blank" rel="noopener noreferrer" color="primary" wideMobile href="https://cyw-corporate-frontend-bbpe-p20fflamb-team-01-frontend.vercel.app/">
                       Live version
-                    </Button>
-                    <Button tag="a" target="_blank" rel="noopener noreferrer" color="dark" wideMobile href="https://github.com/TimmyChan99/Car-rental-app-back-end">
-                      Source Github
                     </Button>
                   </ButtonGroup>
                 </div>
@@ -96,7 +93,7 @@ const FeaturesSplit = ({
                 )}
                 data-reveal-container=".split-item">
                 <Image
-                  src={require('./../../assets/images/rentar.gif')}
+                  src={require('./../../assets/images/cyw.gif')}
                   alt="Features split 02"
                   width={528}
                   height={396} />
@@ -106,21 +103,21 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Ruby on Rails • Ruby • Sass • HTML • RSpec
+                  React • TypeScript • styled-components • Firebase • vite
                 </div>
                 <h3 className="mt-0 mb-12">
-                  myBudget
+                Time Tracker
                   </h3>
                 <p className="m-0">
-                myBudget is a mobile web application where you can manage your budget: you have a list of transactions associated with a category so that you can see how much money you spent and on what.
+                Time Tracker is a web application that allows users to log the hours worked for each day and displays the total number of hours worked. It is designed to be simple and easy to use, with a clean and intuitive interface.
                 </p>
                 <br />
                   <div className="reveal-from-bottom" data-reveal-delay="600">
                     <ButtonGroup>
-                      <Button tag="a" target="_blank" rel="noopener noreferrer" color="primary" wideMobile href="https://mybudget-railsapp.herokuapp.com/">
+                      <Button tag="a" target="_blank" rel="noopener noreferrer" color="primary" wideMobile href="https://time-tracker-delta.vercel.app/">
                         Live version
                       </Button>
-                      <Button tag="a" target="_blank" rel="noopener noreferrer" color="dark" wideMobile href="https://github.com/TimmyChan99/Budget-app/">
+                      <Button tag="a" target="_blank" rel="noopener noreferrer" color="dark" wideMobile href="https://github.com/TimmyChan99/time-tracker/">
                         Source Github
                       </Button>
                     </ButtonGroup>
@@ -133,7 +130,7 @@ const FeaturesSplit = ({
                 )}
                 data-reveal-container=".split-item">
                 <Image
-                  src={require('./../../assets/images/budgetDemo2.gif')}
+                  src={require('./../../assets/images/time-tracker.gif')}
                   alt="Features split 03"
                   width={528}
                   height={396} />
@@ -143,23 +140,21 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  React • Redux • JavaScript • CSS • Jest
+                REACT • JAVASCRIPT • FIREBASE • TAILWIND CSS • REACT-SPRING
                   </div>
                 <h3 className="mt-0 mb-12">
-                COVID-19 Tracker
+                Insight
                   </h3>
                 <p className="m-0">
-                Covid-19 Tracker is a mobile web application that provides the user the new cases of Covid-19 for each country and also the regions within the country by getting all the data and the statistics from Narrativa.
+                This is a blog app where users can read articles and posts of others. If you want to post or write an article you need to register or sign up.
                   </p>
                   <br />
-                  <p className="notice text-color-primary">Notice: Narrativa’s COVID-19 tracking project is no longer providing the API data</p>
-                  <br />
                   <div className="reveal-from-bottom" data-reveal-delay="600">
                     <ButtonGroup>
-                      <Button tag="a" target="_blank" rel="noopener noreferrer" color="primary" wideMobile href="https://timmychan99.github.io/covid19-tracker/">
+                      <Button tag="a" target="_blank" rel="noopener noreferrer" color="primary" wideMobile href="https://timmychan99.github.io/insights/">
                         Live version
                       </Button>
-                      <Button tag="a" target="_blank" rel="noopener noreferrer" color="dark" wideMobile href="https://github.com/TimmyChan99/covid19-tracker">
+                      <Button tag="a" target="_blank" rel="noopener noreferrer" color="dark" wideMobile href="https://github.com/TimmyChan99/insights">
                         Source Github
                       </Button>
                     </ButtonGroup>
@@ -172,7 +167,7 @@ const FeaturesSplit = ({
                 )}
                 data-reveal-container=".split-item">
                 <Image
-                  src={require('./../../assets/images/covid19-2.png')}
+                  src={require('./../../assets/images/insights2.gif')}
                   alt="Features split 01"
                   width={528}
                   height={396} />
@@ -182,22 +177,21 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  React • Redux • JavaScript • CSS • Jest
+                NodeJS • JavaScript • CSS • Axios • HTML
                   </div>
                 <h3 className="mt-0 mb-12">
-                Space Travlers
+                eJob
                   </h3>
                 <p className="m-0">
-                  The application will allow users to book rockets and join selected space missions. Using data from the SpaceX API.
-                  The Space Travelers' Hub consists of Rockets, Missions, and the My Profile section. The My Profile section displays all reserved rockets and space missions.
+                Job search app that list the recent jobs in Canada, UK, USA. the user can get the list of jobs by entering the keyworks related to th position, the location such as city, province or state, and the select the country.
                 </p>
                   <br />
                   <div className="reveal-from-bottom" data-reveal-delay="600">
                     <ButtonGroup>
-                      <Button tag="a" target="_blank" rel="noopener noreferrer" color="primary" wideMobile href="https://timmychan99.github.io/space-travelers/">
+                      <Button tag="a" target="_blank" rel="noopener noreferrer" color="primary" wideMobile href="https://job-search-app-frontend.vercel.app/">
                         Live version
                       </Button>
-                      <Button tag="a" target="_blank" rel="noopener noreferrer" color="dark" wideMobile href="https://github.com/TimmyChan99/space-travelers">
+                      <Button tag="a" target="_blank" rel="noopener noreferrer" color="dark" wideMobile href="https://github.com/TimmyChan99/job-search-app">
                         Source Github
                       </Button>
                     </ButtonGroup>
@@ -210,49 +204,13 @@ const FeaturesSplit = ({
                 )}
                 data-reveal-container=".split-item">
                 <Image
-                  src={require('./../../assets/images/space.gif')}
+                  src={require('./../../assets/images/ejob.gif')}
                   alt="Features split 01"
                   width={528}
                   height={396} />
               </div>
             </div>
 
-            <div className="split-item">
-              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
-                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  JavaScript • CSS • HTML • Webpack • Jest
-                  </div>
-                <h3 className="mt-0 mb-12">
-                To-Do List
-                  </h3>
-                <p className="m-0">
-                To-Do List web app that helps to organize your day. You can add, remove, check the completed tasks, and store your list in your browser (local storage). Build using Webpack and JavaScript.
-                </p>
-                  <br />
-                  <div className="reveal-from-bottom" data-reveal-delay="600">
-                    <ButtonGroup>
-                      <Button tag="a" target="_blank" rel="noopener noreferrer" color="primary" wideMobile href="https://timmychan99.github.io/To_Do_list/">
-                        Live version
-                      </Button>
-                      <Button tag="a" target="_blank" rel="noopener noreferrer" color="dark" wideMobile href="https://github.com/TimmyChan99/To_Do_list">
-                        Source Github
-                      </Button>
-                    </ButtonGroup>
-                  </div>
-              </div>
-              <div className={
-                classNames(
-                  'split-item-image center-content-mobile reveal-from-bottom',
-                  imageFill && 'split-item-image-fill'
-                )}
-                data-reveal-container=".split-item">
-                <Image
-                  src={require('./../../assets/images/todo.gif')}
-                  alt="Features split 01"
-                  width={528}
-                  height={396} />
-              </div>
-            </div>
 
           </div>
         </div>
